29a5d87f3acadb09270baeb94c74edbbe8b72ee2
freestraws <stephdonnelly1@gmail.com>
2019-07-24 19:15:45 -0400

    added more links and images to projects

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+let app = express()
+app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.get('/', (req, res) => res.render('pages/home',
+                        {top:   "academic",
+                        left:   "current",
+                        bottom: "work",
+                        right:  "other",
+                        center: "home"}))
+
+app.get('/other', (req, res) => res.render('pages/other'))
+app.get('/academic', (req, res) => res.render('pages/academic',
+                        {top:   "academic",
+                        left:   "current",
+                        bottom: "work",
+                        right:  "other",
+                        center: "home"}))
+app.get('/squares', (req, res) => res.render('pages/squares',
+                        {top:   "academic",
+                        left:   "current",
+                        bottom: "work",
+                        right:  "other",
+                        center: "home"}))
+app.get('/work', (req, res) => res.render('pages/work'))
+app.get('/current', (req, res) => res.render('pages/current'))
--- public/lib/compass.js
+++ public/lib/compass.js
@@ -3,12 +3,14 @@ $(function() {
   var left = -90;
   var top = 0;
   var bottom = 180;
+  var center = 20;
   var setDirection = 0;
   switch($(".current").attr('id')){
     case 'left': setDirection = left; break;
     case 'right': setDirection = right; break;
     case 'top': setDirection = top; break;
     case 'bottom': setDirection = bottom; break;
+    case 'center': setDirection = center; break;
     default: setDirection = 0; break;
   }
   document.styleSheets[1].addRule('nav:before','transform: rotate('+setDirection+'deg);');
@@ -29,6 +31,11 @@ $(function() {
   $('#bottom').hover(function(){
     document.styleSheets[1].addRule('nav:before','transform: rotate('+bottom+'deg);');
     $('.center').text($('#bottom a .hover').text());
+  });
+
+  $('#center').hover(function(){
+    document.styleSheets[1].addRule('nav:before','transform: rotate('+center+'deg);');
+    $('.center').text($('#center a .hover').text());
   });
 
   $('nav').mouseleave(function(){
